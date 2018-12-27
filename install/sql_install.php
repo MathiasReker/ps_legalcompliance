4c1e7896a0b5db8eed6e50146522f13955d21319
MathiasReker <mathias@reker.dk>
2018-12-27 15:51:19 +0100

    No blank lines after phpdoc

--- install/sql_install.php
+++ install/sql_install.php
@@ -23,7 +23,6 @@
  *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  */
-
 $sql = array();
 
 /* CMS Role to Mail association */
